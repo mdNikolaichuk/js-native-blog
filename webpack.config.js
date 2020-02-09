fc7216d5ead20d1d5a22ba924cc317b602e513a6
Maksym <35653052+mdNikolaichuk@users.noreply.github.com>
2020-02-09 14:19:45 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HTMLPlugin = require ('html-webpack-plugin')
+
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer:{
+        contentBase: __dirname + '/dist'
+    },
+    plugins:[
+        new HTMLPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    resolve:{
+        extensions:['.js']
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+}
